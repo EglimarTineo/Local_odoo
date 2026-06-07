0dd170620e1f7939417cf60463164ee93b6a73bf
EglimarTineo <eglimart22@gmail.com>
2026-06-07 18:57:44 -0400

    Update account_move.py

--- l10n_ve_sumitic/models/account_move.py
+++ l10n_ve_sumitic/models/account_move.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from odoo import models, fields
-from odoo.orm import Constraint
+
 
 class AccountMove(models.Model):
     _inherit = 'account.move'
@@ -16,11 +16,3 @@ class AccountMove(models.Model):
         ('special', 'Especial'),
         ('ordinary', 'Ordinario'),
     ], string="Tipo de Contribuyente", store=True)
-
-    constraints = [
-        Constraint(
-            'unique_control_number',
-            'unique(l10n_ve_control_number, company_id, move_type)',
-            'El número de control debe ser único por tipo de documento.'
-        ),
-    ]

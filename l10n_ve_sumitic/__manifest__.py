7561449ed0b35c1b80f67a684fc701b06a91d64f
EglimarTineo <eglimart22@gmail.com>
2026-06-07 18:42:36 -0400

    Update __manifest__.py

--- l10n_ve_sumitic/__manifest__.py
+++ l10n_ve_sumitic/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Venezuela - Contabilidad Sumitic',
-    'version': '19.0',
+    'version': '19.0.1.0.0',
     'summary': 'Localización Contable para Venezuela adaptada para Odoo 19',
     'category': 'Accounting/Localizations/Account Chart',
     'author': 'Sumitic',
@@ -9,7 +9,6 @@
     'depends': [
         'account',
         'base_vat',
-        'l10n_ve',
     ],
     'data': [
         'security/ir.model.access.csv',
